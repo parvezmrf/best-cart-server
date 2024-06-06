53a8ec0796609542bc4015b5df3e44f9c3ca25ca
unknown <parvezmrf@gmail.com>
2024-06-06 10:47:55 +0600

    server update

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worlddddd!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app;
